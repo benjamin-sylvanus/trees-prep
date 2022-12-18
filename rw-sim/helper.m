947a188619d973ac0b0584143a71cc3b19152eb7
Benjamin Sylvanus <sylvanus.b@northeastern.edu>
2022-12-18 01:57:31 -0800

    rebs

--- rw-sim/helper.m
+++
@@ -1,6 +0,0 @@
-function temp = helper(temp, inds, i)
-
-    k = repelem(i, size(temp, 1));
-    temp(:, end + 1) = {i};
-    temp
-end
--- rw-sim/helper2.m
+++
@@ -1,8 +0,0 @@
-function [inds] = helper2 (bsize, ci)
-
-    try
-        inds = sub2ind(bsize, ci(:, 1), ci(:, 2), ci(:, 3));
-    catch
-    end
-
-end
